1ec3a6503bc845afbd9b54d702b574e2add049f0
arif wijanarko <arifpethod@gmail.com>
2023-06-16 08:43:56 +0700

    tugas js 02
    Tugas Untuk Niomic

---
+++ tugas2.js
@@ -0,0 +1,19 @@
+var number = 2020;
+
+var string = "arif wijanarko";
+
+var boolean = 20;
+if (boolean < 100) {
+    boolean = true;
+} else {
+    boolean = false;
+}
+
+var empty = "able";
+empty = null;
+
+var unknown;
+
+var arr = [1, "arif", 2, false];
+
+console.log(number, string, boolean, empty, unknown, arr);
